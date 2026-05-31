fb2cc84f8b7203e2ec8b10d3b4ed009a9272efa8
Denis Galkin <dis43@mail.ru>
2026-05-31 19:01:14 +0300

    add .flake8 poetry

---
+++ src/masks.py
@@ -0,0 +1,9 @@
+def get_mask_card_number(card_number: str) -> str:
+    """Функцию маскировки номера банковской карты"""
+    return f"{card_number[:7]}** ****{card_number[-4:]}"
+
+
+def get_mask_account(account_number: str) -> str:
+    """Функцию маскировки номера банковского счета"""
+    return f"** {account_number[-4:]}"
+
