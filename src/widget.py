2e472aa5b24f6779a2611c1817fb883d104cc8b2
Denis Galkin <dis43@mail.ru>
2026-06-01 19:56:03 +0300

    bug fix in get_date and add docstring

--- src/widget.py
+++ src/widget.py
@@ -2,6 +2,7 @@ from src.masks import get_mask_card_number, get_mask_account
 
 
 def mask_account_card(info_card: str) -> str:
+    """ Функция маскировки банковской карты (с названием карты) """
     name_chars = []
     number_chars = []
     for symbol in info_card:
@@ -20,4 +21,4 @@ def mask_account_card(info_card: str) -> str:
 
 
 def get_date(str_date: str) -> str:
-    return f"{str_date[8:10]}, {str_date[5:7]}, {str_date[0:4]}"
+    return f"{str_date[8:10]}.{str_date[5:7]}.{str_date[0:4]}"
