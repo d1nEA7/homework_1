2f470aca4b06b5c36aaf66bef3ddd8a688314067
Denis Galkin <dis43@mail.ru>
2026-06-08 14:38:58 +0300

    add processing.py and add 2 def filter_by_state and sort_by_date

--- src/processing.py
+++ src/processing.py
@@ -1,6 +1,12 @@
 def filter_by_state(list_dict: list, state:str="EXECUTED") -> list:
+    """ Функция возвращает новый список словарей по значению EXECUTED"""
     new_list_dict = []
     for item in list_dict:
-        if item.get("state") == "EXECUTED":
+        if item.get("state") == state:
+            new_list_dict.append(item)
+    return new_list_dict
 
 
+def sort_by_date(list_dict: list, reverse=True) -> list:
+    """Функция возвращает новый список, отсортированный по дате"""
+    return sorted(list_dict, key=lambda x: x["date"], reverse=reverse)
