a337e404ce2176f75435da4eb14597ea4a637c25
d1nEA7 <dis43@mail.ru>
2026-06-21 19:10:49 +0300

    Add generator info operations

--- src/generators.py
+++ src/generators.py
@@ -6,4 +6,7 @@ def filter_by_currency(transactions:list, currency:str) -> Iterator[dict]:
         if transaction.get("operationAmount", {}).get("currency", {}).get("code") == currency:
             yield transaction
 
-
+def transaction_descriptions(transactions:list) -> Iterator[str]:
+    """Генератор описания каждой операции по очереди"""
+    for transaction in transactions:
+        yield transaction.get("description", "Описание отсутствует")
