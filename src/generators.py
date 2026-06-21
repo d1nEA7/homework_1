b809c78f4a97577ab55f9b93c035902fbff798c0
d1nEA7 <dis43@mail.ru>
2026-06-21 19:40:14 +0300

    add generator numb card

--- src/generators.py
+++ src/generators.py
@@ -10,3 +10,13 @@ def transaction_descriptions(transactions:list) -> Iterator[str]:
     """Генератор описания каждой операции по очереди"""
     for transaction in transactions:
         yield transaction.get("description", "Описание отсутствует")
+
+def card_number_generator(num_start:int, num_end:int) -> Iterator[str]:
+    """генератор номера карт"""
+    for num in range(num_start, num_end + 1):
+        card_str = str(num)
+        while len(card_str) < 16:
+            card_str = "0" + card_str
+        result = f"{card_str[:4]} {card_str[4:8]} {card_str[8:12]} {card_str[12:]}"
+        yield result
+
