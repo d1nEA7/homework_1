6d2b022799c9bc3fe4ac08fb05e1cb48a1e4083f
Denis Galkin <dis43@mail.ru>
2026-07-05 22:09:40 +0300

    poetry run black mypy isort flake8

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,10 +6,15 @@ import pytest
 
 @pytest.fixture(autouse=True, scope="session")
 def set_env():
+    """Устанавливает переменную окружения API_KEY для всех тестов."""
     os.environ["API_KEY"] = "some-secret-key"
 
 
 @pytest.fixture
 def mocked_request_get():
+    """
+    Фикстура, подменяющая requests.get на "мок".
+    Используется в тестах для имитации ответов API без реальных запросов.
+    """
     with patch("requests.get") as mock:
         yield mock
