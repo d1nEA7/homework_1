88302efad5cc923493a97cbc977d1f249b653811
d1nEA7 <dis43@mail.ru>
2026-07-12 14:18:19 +0300

    add tests for new funcs

--- src/transaction_reader.py
+++ src/transaction_reader.py
@@ -1,14 +1,26 @@
-import pandas as pd
+from pathlib import Path
 
+import pandas as pd
 
 
 def read_transactions_csv(path: str = "src/transactions.csv") -> list[dict]:
     """Функция чтения CSV файла транзакций."""
-    df = pd.read_csv(path)
-    return df.to_dict(orient='records')
+    if not Path(path).exists():
+        raise FileNotFoundError(f"Файл не найден: {path}")
+
+    df = pd.read_csv(path, sep=";")  # ← добавлен sep=';'
+    return df.to_dict(orient="records")
 
 
 def read_transactions_excel(path: str = "src/transactions_excel.xlsx") -> list[dict]:
     """Функция чтения Excel файла транзакций."""
+    if not Path(path).exists():
+        raise FileNotFoundError(f"Файл не найден: {path}")
+
     df = pd.read_excel(path)
-    return df.to_dict(orient='records')
+    return df.to_dict(orient="records")
+
+
+if __name__ == "__main__":
+    data = read_transactions_excel("src/transactions_excel.xlsx")
+    print(data)
---
+++ tests/test_transaction_reader.py
@@ -0,0 +1,47 @@
+from unittest.mock import patch
+
+import pandas as pd
+import pytest
+
+from src.transaction_reader import read_transactions_csv, read_transactions_excel
+
+
+@pytest.fixture
+def sample_transactions():
+    """фикстура для теста входных данных о транзакциях"""
+    return [
+        {
+            "id": 650703.0,
+            "state": "EXECUTED",
+            "date": "2023-09-05T11:30:32Z",
+            "amount": 16210.0,
+            "currency_name": "Sol",
+            "currency_code": "PEN",
+            "from": "Счет 58803664561298323391",
+            "to": "Счет 39745660563456619397",
+            "description": "Перевод организации",
+        },
+    ]
+
+
+@patch("pandas.read_csv")
+@patch("pathlib.Path.exists")
+def test_read_transactions_csv(mock_exists, mock_read_csv, sample_transactions):
+    """проверка мок и патч на работу чтения csv файла"""
+    mock_exists.return_value = True  # файл существует
+    mock_df = pd.DataFrame(sample_transactions)
+    mock_read_csv.return_value = mock_df
+    result = read_transactions_csv("fake.csv")
+    assert result == sample_transactions
+    mock_read_csv.assert_called_once_with("fake.csv", sep=";")
+
+
+@patch("pandas.read_excel")
+@patch("pathlib.Path.exists")
+def test_read_transactions_excel(mock_exists, mock_read_excel, sample_transactions):
+    """Тест чтения Excel-файла с транзакциями"""
+    mock_df = pd.DataFrame(sample_transactions)  # ← создаём DataFrame из фикстуры
+    mock_read_excel.return_value = mock_df
+    result = read_transactions_excel("fake.xlsx")
+    assert result == sample_transactions
+    mock_read_excel.assert_called_once_with("fake.xlsx")
