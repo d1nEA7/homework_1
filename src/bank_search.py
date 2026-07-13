b24cc332039aa685ce5098eed975798485af8369
d1nEA7 <dis43@mail.ru>
2026-07-13 23:33:38 +0300

    finish

--- src/bank_search.py
+++ src/bank_search.py
@@ -15,9 +15,10 @@ def process_bank_operations(data: list[dict], categories: list) -> dict:
     """принимает список словарей с данными о банковских операциях и список категорий операций, а возвращает словарь,
     в котором ключи — это названия категорий, а значения — это количество операций в каждой категории."""
     counts: Counter[str] = Counter()
+    result = {category: 0 for category in categories}
     for transaction in data:
         description = transaction.get("description", "")
         for category in categories:
             if category.lower() in description.lower():
-                counts[category] += 1
-    return dict(counts)
+                result[category] += 1
+    return result
