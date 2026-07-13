59a8150b7a187aa15eaef9497f62286a2aa06190
d1nEA7 <dis43@mail.ru>
2026-07-13 15:54:59 +0300

    add func process_bank_operations

--- src/bank_search.py
+++ src/bank_search.py
@@ -1,5 +1,5 @@
 import re
-
+from collections import Counter
 
 def process_bank_search(data: list[dict], search: str) -> list[dict]:
     """Функция поиска по данным в транзакциях, возвращает список словарей"""
@@ -8,3 +8,12 @@ def process_bank_search(data: list[dict], search: str) -> list[dict]:
         if re.search(search, item.get('description', ''), re.IGNORECASE):
             result.append(item)
     return result
+
+def process_bank_operations(data:list[dict], categories:list)->dict:
+    """принимает список словарей с данными о банковских операциях и список категорий операций, а возвращает словарь,
+     в котором ключи — это названия категорий, а значения — это количество операций в каждой категории."""
+    descriptions = [operation['description'] for operation in data]
+    count = Counter(descriptions)
+    result = {category: count[category] for category in categories}
+    return result
+
